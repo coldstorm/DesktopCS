5b60355a047a2ad41362c7d38d50f328a5217161
Express50 <press.ex50@gmail.com>
2013-05-04 16:38:35 -0400

    Added RichTextBox to each tab and AddLine method

--- DesktopCS/Forms/MainForm.cs
+++ DesktopCS/Forms/MainForm.cs
@@ -18,6 +18,10 @@ namespace DesktopCS.Forms
             AddTab("test");
             AddTab("test2");
 
+            AddLine(0, "test");
+            AddLine(1, "test");
+            AddLine(0, "test");
+
             BackColor = Constants.BACKGROUND_COLOR;
             ForeColor = Constants.TEXT_COLOR;
             MainMenuStrip.BackColor = BackColor;
@@ -31,12 +35,31 @@ namespace DesktopCS.Forms
         private void AddTab(string title)
         {
             CTabPage Tab = new CTabPage(title);
+
+            //Prepare RichTextBox
+            RichTextBox TextBox = new RichTextBox();
+            TextBox.Name = "TextBox";
+            TextBox.Dock = DockStyle.Fill;
+            TextBox.BorderStyle = BorderStyle.None;
+            TextBox.BackColor = Constants.CHAT_BACKGROUND_COLOR;
+            TextBox.ForeColor = Constants.TEXT_COLOR;
+            TextBox.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            TextBox.ReadOnly = true;
+
+            Tab.Controls.Add(TextBox);
             cTabControl.TabPages.Add(Tab);
         }
 
         private void RemoveTab(int index)
         {
             cTabControl.TabPages.RemoveAt(index);
+        }
+
+        private void AddLine(int tabIndex, string line)
+        {
+            line = line.Trim();
+            string formattedline = DateTime.Now.ToString("[HH:mm] ") + line + "\n";
+            cTabControl.TabPages[tabIndex].Controls["TextBox"].Text += formattedline;
         }
     }
 }
