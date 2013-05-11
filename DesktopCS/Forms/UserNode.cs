09299e90cfeca99d233d8651b6e4526f05f09b65
Kevin Brown <bass5098@gmail.com>
2013-05-11 17:49:58 -0400

    Working on getting the user colors

--- DesktopCS/Forms/UserNode.cs
+++ DesktopCS/Forms/UserNode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using NetIRC;
 
@@ -23,6 +24,29 @@ namespace DesktopCS.Forms
         {
             this.Text = this.RankChars[user.Rank] + user.NickName;
             this.User = user;
+
+            this.ForeColor = this.ColorFromUser(user);
+        }
+
+        private Color ColorFromUser(User user)
+        {
+            string colorCode = User.UserName;
+
+            if (colorCode.Length != 8)
+            {
+                return this.ForeColor;
+            }
+
+            colorCode = colorCode.Substring(0, 6);
+
+            try
+            {
+                return ColorTranslator.FromHtml("#" + colorCode);
+            }
+            catch (FormatException e)
+            {
+                return this.ForeColor;
+            }
         }
     }
 }
