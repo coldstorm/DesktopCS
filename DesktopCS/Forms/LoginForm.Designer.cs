064fe1b634a6b5e64af74509fa03d4cff682b1db
Kevin Brown <bass5098@gmail.com>
2013-05-12 19:53:52 -0400

    Change LoginForm design to better match the web client

--- DesktopCS/Forms/LoginForm.Designer.cs
+++ DesktopCS/Forms/LoginForm.Designer.cs
@@ -40,62 +40,73 @@
             // 
             // NicknameBox
             // 
-            this.NicknameBox.Location = new System.Drawing.Point(116, 47);
+            this.NicknameBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.NicknameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NicknameBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(187)))), ((int)(((byte)(191)))));
+            this.NicknameBox.Location = new System.Drawing.Point(40, 40);
             this.NicknameBox.Name = "NicknameBox";
-            this.NicknameBox.Size = new System.Drawing.Size(100, 20);
+            this.NicknameBox.Size = new System.Drawing.Size(190, 22);
             this.NicknameBox.TabIndex = 0;
             // 
             // NicknameLabel
             // 
             this.NicknameLabel.AutoSize = true;
-            this.NicknameLabel.Location = new System.Drawing.Point(140, 31);
+            this.NicknameLabel.Location = new System.Drawing.Point(99, 15);
             this.NicknameLabel.Name = "NicknameLabel";
-            this.NicknameLabel.Size = new System.Drawing.Size(55, 13);
+            this.NicknameLabel.Size = new System.Drawing.Size(72, 14);
             this.NicknameLabel.TabIndex = 1;
             this.NicknameLabel.Text = "Nickname";
             // 
             // PasswordLabel
             // 
             this.PasswordLabel.AutoSize = true;
-            this.PasswordLabel.Location = new System.Drawing.Point(140, 69);
+            this.PasswordLabel.Location = new System.Drawing.Point(99, 71);
             this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.Size = new System.Drawing.Size(72, 14);
             this.PasswordLabel.TabIndex = 3;
             this.PasswordLabel.Text = "Password";
             // 
             // PasswordBox
             // 
-            this.PasswordBox.Location = new System.Drawing.Point(116, 85);
+            this.PasswordBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.PasswordBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PasswordBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(187)))), ((int)(((byte)(191)))));
+            this.PasswordBox.Location = new System.Drawing.Point(40, 96);
             this.PasswordBox.Name = "PasswordBox";
-            this.PasswordBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordBox.Size = new System.Drawing.Size(190, 22);
             this.PasswordBox.TabIndex = 2;
             this.PasswordBox.UseSystemPasswordChar = true;
             // 
             // ColorLabel
             // 
             this.ColorLabel.AutoSize = true;
-            this.ColorLabel.Location = new System.Drawing.Point(153, 107);
+            this.ColorLabel.Location = new System.Drawing.Point(114, 127);
             this.ColorLabel.Name = "ColorLabel";
-            this.ColorLabel.Size = new System.Drawing.Size(31, 13);
+            this.ColorLabel.Size = new System.Drawing.Size(42, 14);
             this.ColorLabel.TabIndex = 5;
             this.ColorLabel.Text = "Color";
             // 
             // ColorBox
             // 
-            this.ColorBox.Location = new System.Drawing.Point(116, 123);
+            this.ColorBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.ColorBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(187)))), ((int)(((byte)(191)))));
+            this.ColorBox.Location = new System.Drawing.Point(40, 150);
             this.ColorBox.Name = "ColorBox";
-            this.ColorBox.Size = new System.Drawing.Size(100, 20);
+            this.ColorBox.Size = new System.Drawing.Size(190, 22);
             this.ColorBox.TabIndex = 4;
             this.ColorBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ColorBox_MouseDoubleClick);
             // 
             // LoginButton
             // 
-            this.LoginButton.Location = new System.Drawing.Point(130, 149);
+            this.LoginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(46)))));
+            this.LoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginButton.Location = new System.Drawing.Point(83, 183);
             this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.Size = new System.Drawing.Size(105, 26);
             this.LoginButton.TabIndex = 6;
             this.LoginButton.Text = "Login";
-            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.UseVisualStyleBackColor = false;
             // 
             // ColorChooser
             // 
@@ -104,9 +115,10 @@
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 213);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(271, 220);
             this.Controls.Add(this.LoginButton);
             this.Controls.Add(this.ColorLabel);
             this.Controls.Add(this.ColorBox);
@@ -114,7 +126,12 @@
             this.Controls.Add(this.PasswordBox);
             this.Controls.Add(this.NicknameLabel);
             this.Controls.Add(this.NicknameBox);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(187)))), ((int)(((byte)(191)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login to Coldstorm";
             this.ResumeLayout(false);
             this.PerformLayout();
--- DesktopCS/Forms/LoginForm.cs
+++ DesktopCS/Forms/LoginForm.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace DesktopCS.Forms
 {
     public partial class LoginForm : Form
     {
+        private bool fullClose = true;
+
         public LoginForm()
         {
             InitializeComponent();
 
             this.NicknameBox.Text = Properties.Settings.Default.Nickname;
             this.ColorBox.Text = Properties.Settings.Default.Color;
+
+            //this.Load += LoginForm_Load;
+            this.FormClosing += LoginForm_FormClosing;
         }
 
-        private void ColorBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        protected void ColorBox_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             ColorChooser.Color = ColorTranslator.FromHtml(this.ColorBox.Text);
             DialogResult result = ColorChooser.ShowDialog();
@@ -26,8 +32,17 @@ namespace DesktopCS.Forms
             }
         }
 
+        protected void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (fullClose)
+            {
+                Application.Exit();
+            }
+        }
+
         protected void LoginForm_Load(object sender, System.EventArgs e)
         {
+            this.fullClose = false;
             this.Close();
 
             MainForm main = new MainForm();
