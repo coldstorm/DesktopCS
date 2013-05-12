15dd15b9d2176040885286ab0e812bd8d102f35b
Express50 <press.ex50@gmail.com>
2013-05-12 18:18:46 -0400

    Started working on LoginForm

---
+++ DesktopCS/Forms/LoginForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DesktopCS.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NicknameBox = new System.Windows.Forms.TextBox();
+            this.NicknameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.ColorLabel = new System.Windows.Forms.Label();
+            this.ColorBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NicknameBox
+            // 
+            this.NicknameBox.Location = new System.Drawing.Point(116, 47);
+            this.NicknameBox.Name = "NicknameBox";
+            this.NicknameBox.Size = new System.Drawing.Size(100, 20);
+            this.NicknameBox.TabIndex = 0;
+            // 
+            // NicknameLabel
+            // 
+            this.NicknameLabel.AutoSize = true;
+            this.NicknameLabel.Location = new System.Drawing.Point(140, 31);
+            this.NicknameLabel.Name = "NicknameLabel";
+            this.NicknameLabel.Size = new System.Drawing.Size(55, 13);
+            this.NicknameLabel.TabIndex = 1;
+            this.NicknameLabel.Text = "Nickname";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(140, 69);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(116, 85);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordBox.TabIndex = 2;
+            this.PasswordBox.UseSystemPasswordChar = true;
+            // 
+            // ColorLabel
+            // 
+            this.ColorLabel.AutoSize = true;
+            this.ColorLabel.Location = new System.Drawing.Point(153, 107);
+            this.ColorLabel.Name = "ColorLabel";
+            this.ColorLabel.Size = new System.Drawing.Size(31, 13);
+            this.ColorLabel.TabIndex = 5;
+            this.ColorLabel.Text = "Color";
+            // 
+            // ColorBox
+            // 
+            this.ColorBox.Location = new System.Drawing.Point(116, 123);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(100, 20);
+            this.ColorBox.TabIndex = 4;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(130, 149);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 6;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 213);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.ColorLabel);
+            this.Controls.Add(this.ColorBox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.NicknameLabel);
+            this.Controls.Add(this.NicknameBox);
+            this.Name = "LoginForm";
+            this.Text = "Login to Coldstorm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NicknameBox;
+        private System.Windows.Forms.Label NicknameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label ColorLabel;
+        private System.Windows.Forms.TextBox ColorBox;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
