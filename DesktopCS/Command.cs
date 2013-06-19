590e6fd3314e614d677d7585055a1608fe40c9f0
Express50 <press.ex50@gmail.com>
2013-06-18 20:31:30 -0400

    Added Command class

---
+++ DesktopCS/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetIRC;
+
+namespace DesktopCS
+{
+    public class Command
+    {
+        public string Name;
+        public int MinParams;
+        public delegate void callback(Client sender, string[] parameters);
+        public callback Callback;
+        public string Usage;
+
+        public Command(string name, int minparams, callback cback, string usage)
+        {
+            Name = name;
+            MinParams = minparams;
+            this.Callback = cback;
+            Usage = usage;
+        }
+    }
+}
