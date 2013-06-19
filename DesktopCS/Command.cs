96b8ef0de9ec30c6cf91f18d47ae585d71af632b
Express50 <press.ex50@gmail.com>
2013-06-18 21:04:30 -0400

    Converted List of commands to a Dictionary for easier lookup. Added ParameterException throwing in callbacks.

--- DesktopCS/Command.cs
+++ DesktopCS/Command.cs
@@ -9,15 +9,13 @@ namespace DesktopCS
 {
     public class Command
     {
-        public string Name;
         public int MinParams;
         public delegate void callback(Client sender, string[] parameters);
         public callback Callback;
         public string Usage;
 
-        public Command(string name, int minparams, callback cback, string usage)
+        public Command(int minparams, callback cback, string usage)
         {
-            Name = name;
             MinParams = minparams;
             this.Callback = cback;
             Usage = usage;
--- DesktopCS/CommandExecutor.cs
+++ DesktopCS/CommandExecutor.cs
@@ -9,40 +9,55 @@ namespace DesktopCS
 {
     public class CommandExecutor
     {
-        private List<Command> Commands;
+        private Dictionary<string, Command> Commands;
 
         public CommandExecutor()
         {
-            Commands.Add(new Command("join", 2, JoinCallback, "/join <channel>"));
-            Commands.Add(new Command("part", 2, PartCallback, "/part <channel>"));
-            Commands.Add(new Command("topic", 3, TopicCallback, "/topic <channel> <text>"));
+            Commands.Add("join", new Command(2, JoinCallback, "/join <channel>"));
+            Commands.Add("part", new Command(2, PartCallback, "/part <channel>"));
+            Commands.Add("topic", new Command(3, TopicCallback, "/topic <channel> <text>"));
         }
 
         public void Execute(Client invoker, string command)
         {
             string[] parts = command.Remove(0, 1).Trim().Split(' ');
 
-            foreach (Command cmd in Commands)
+            foreach (KeyValuePair<string, Command> entry in Commands)
             {
-                if (cmd.Name == parts[0].ToLowerInvariant())
+                if (entry.Key == parts[0].ToLowerInvariant())
                 {
-                    cmd.Callback(invoker, parts);
+                    entry.Value.Callback(invoker, parts);
                 }
             }
         }
 
         private void JoinCallback(Client sender, string[] parameters)
         {
+            if (parameters.Length < Commands["join"].MinParams)
+            {
+                throw new ParameterException();
+            }
+
             sender.JoinChannel(parameters[1]);
         }
 
         private void PartCallback(Client sender, string[] parameters)
         {
+            if (parameters.Length < Commands["part"].MinParams)
+            {
+                throw new ParameterException();
+            }
+
             sender.LeaveChannel(parameters[1]);
         }
 
         private void TopicCallback(Client sender, string[] parameters)
         {
+            if (parameters.Length < Commands["topic"].MinParams)
+            {
+                throw new ParameterException();
+            }
+
             string topic = string.Join(" ", parameters.Skip(2));
             sender.Channels[parameters[1]].SetTopic(topic);
         }
