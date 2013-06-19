c036c3d2ca96124b19da0cef769b06a1a969a7cf
Express50 <press.ex50@gmail.com>
2013-06-18 20:34:21 -0400

    Added ParameterException

---
+++ DesktopCS/ParameterException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCS
+{
+    public class ParameterException : Exception
+    {
+        public ParameterException() : base() {}
+        public ParameterException(string message) : base(message) { }
+    }
+}
