f783a1204e9755fae83c64d767e92693a4360108
Yonom <sepehr1376@gmail.com>
2013-08-07 14:16:09 +0200

    Add custom UserlistItem control

---
+++ DesktopCS/Controls/UserlistItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopCS.Controls
+{
+    public class UserlistItem : ContentControl
+    {
+        static UserlistItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(UserlistItem), new FrameworkPropertyMetadata(typeof(UserlistItem)));
+        }
+
+        public static DependencyProperty FlagProperty = DependencyProperty.Register("Flag", typeof(Image), typeof(UserlistItem));
+
+        [System.ComponentModel.Description("Flag")]
+        [System.ComponentModel.Category("Common")]
+        public Image Flag
+        {
+            get { return (Image)GetValue(FlagProperty); }
+            set { SetValue(FlagProperty, value); }
+        }
+    }
+}
