db1f443ee110090056aa377e4cd30e7d29d27c56
Yonom <sepehr1376@gmail.com>
2013-08-11 03:38:32 +0200

    Add Test project

---
+++ DesktopCS.Tests/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesktopCS.Tests
+{
+    [TestClass]
+    public class Helpers
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
