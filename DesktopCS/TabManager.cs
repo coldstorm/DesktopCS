0174fbd29ca59c0bb672b32a54e670376c2c49de
Express50 <press.ex50@gmail.com>
2013-08-11 17:09:48 -0400

    Add TabManager class

---
+++ DesktopCS/TabManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DesktopCS.Controls;
+using DesktopCS.UserControls;
+
+namespace DesktopCS
+{
+    public class TabManager
+    {
+        private ObservableCollection<CSTabItem> _tabs;
+
+        public TabManager(ObservableCollection<CSTabItem> tabs)
+        {
+            this._tabs = tabs;
+        }
+
+        public TabUserControl this[string tabName]
+        {
+            get
+            {
+                return (TabUserControl)_tabs.FirstOrDefault(t => (string)t.Header == tabName).Content;
+            }
+        }
+
+        public void MarkUnread(string tabName)
+        {
+            _tabs.FirstOrDefault(t => (string)t.Header == tabName).Unread = true;
+        }
+    }
+}
