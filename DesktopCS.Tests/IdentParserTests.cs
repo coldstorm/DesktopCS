9553fef4e5943d51b4d84a58308a4f48e0f093c3
Yonom <sepehr1376@gmail.com>
2013-08-11 04:22:57 +0200

    Add 7 tests for IdentParser

---
+++ DesktopCS.Tests/IdentParserTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Windows.Media;
+using DesktopCS.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesktopCS.Tests
+{
+    [TestClass]
+    public class IdentParserTests
+    {
+        [TestMethod]
+        public void InvalidIdentTest()
+        {
+            // arrange
+            const string testIdent = "RANDOM_IDENT";
+            ArgumentException expectedExcetpion = null;
+
+            // act
+            try
+            {
+                IdentParser.Parse(testIdent);
+            }
+            catch (ArgumentException ex)
+            {
+                expectedExcetpion = ex;
+            }
+            
+
+            // assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void EmptyIdentTest()
+        {
+            // arrange
+            const string testIdent = "";
+            ArgumentException expectedExcetpion = null;
+
+            // act
+            try
+            {
+                IdentParser.Parse(testIdent);
+            }
+            catch (ArgumentException ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void NullIdentTest()
+        {
+            // arrange
+            const string testIdent = null;
+            ArgumentException expectedExcetpion = null;
+
+            // act
+            try
+            {
+                IdentParser.Parse(testIdent);
+            }
+            catch (ArgumentException ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void InvalidHexIdentTest()
+        {
+            // arrange
+            const string testIdent = "GGGQQ";
+            ArgumentException expectedExcetpion = null;
+
+            // act
+            try
+            {
+                IdentParser.Parse(testIdent);
+            }
+            catch (ArgumentException ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void TooShortHexIdentTest()
+        {
+            // arrange
+            const string testIdent = "12345QQ";
+            ArgumentException expectedExcetpion = null;
+
+            // act
+            try
+            {
+                IdentParser.Parse(testIdent);
+            }
+            catch (ArgumentException ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void ShortHexIdentTest()
+        {
+            // arrange
+            const string testIdent = "FFFQQ";
+
+            // act
+            var userMetadata = IdentParser.Parse(testIdent);
+
+            // assert
+            Assert.Equals(userMetadata.CountryCode, "QQ");
+            Assert.Equals(userMetadata.Color, Brushes.White);
+        }
+
+        [TestMethod]
+        public void LongHexIdentTest()
+        {
+            // arrange
+            const string testIdent = "FFFFFFQQ";
+
+            // act
+            var userMetadata = IdentParser.Parse(testIdent);
+
+            // assert
+            Assert.Equals(userMetadata.CountryCode, "QQ");
+            Assert.Equals(userMetadata.Color, Brushes.White);
+        }
+
+    }
+}
