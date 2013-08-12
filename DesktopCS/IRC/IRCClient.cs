12b7b98379d44e7dd1eafccb7c264614d22db214
Yonom <sepehr1376@gmail.com>
2013-08-12 21:01:27 +0200

    Move icon to /Resources, Add the incomplete IRCClient class

---
+++ DesktopCS/IRC/IRCClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetIRC;
+
+namespace DesktopCS
+{
+    class IRCClient
+    {
+        public IRCClient()
+        {
+            var irc = new Client();
+        }
+
+    }
+}
