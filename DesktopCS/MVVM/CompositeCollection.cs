0a9368086da294586941960e9fa3c49e171e8cbc
Yonom <sepehr1376@gmail.com>
2013-09-07 19:30:57 +0200

    Revert "Remove custom CompositeCollection"
    This reverts commit 3a763f7ab643fe53ae096e8c7e8a4109189260b3.
    
    Conflicts:
    	DesktopCS/Views/MainView.xaml

---
+++ DesktopCS/MVVM/CompositeCollection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace DesktopCS.MVVM
+{
+    public class CompositeCollection<T> : IEnumerable<T>, INotifyCollectionChanged
+    {
+        private readonly ObservableCollection<T>[] _subCollections;
+
+        public CompositeCollection(params ObservableCollection<T>[] subCollections)
+        {
+            _subCollections = subCollections;
+            SubscribeToSubCollectionChanges();
+        }
+
+        private void SubscribeToSubCollectionChanges()
+        {
+            foreach (var collection in _subCollections)
+            {
+                collection.CollectionChanged += OnSubCollectionChanged;
+            }
+        }
+
+        private void OnSubCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var collection = (ObservableCollection<T>)sender;
+            int startingIndex = 0;
+
+            foreach (var c in _subCollections)
+            {
+                if (c != collection)
+                    startingIndex += c.Count();
+                else
+                    break;
+            }
+
+            NotifyCollectionChangedEventArgs args;
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    args = new NotifyCollectionChangedEventArgs(e.Action, e.NewItems[0], e.NewStartingIndex + startingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    args = new NotifyCollectionChangedEventArgs(e.Action, e.OldItems[0], e.OldStartingIndex + startingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    args = new NotifyCollectionChangedEventArgs(e.Action, e.NewItems, e.NewStartingIndex + startingIndex, e.OldStartingIndex + startingIndex);
+                    break;
+                case NotifyCollectionChangedAction.Replace: 
+                    args = new NotifyCollectionChangedEventArgs(e.Action, e.NewItems, e.OldItems);
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    args = new NotifyCollectionChangedEventArgs(e.Action);
+                    break;
+                default:
+                    args = null;
+                    break;
+            }
+
+           this.OnCollectionChanged(args);
+        }
+
+        #region IEnumerable Memebers
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this._subCollections.SelectMany(collection => collection).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
+
+        #region INotifyCollectionChanged Memebers
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            NotifyCollectionChangedEventHandler handler = this.CollectionChanged;
+            if (handler != null) handler(this, e);
+        }
+
+        #endregion
+    }
+}
--- DesktopCS/Services/TabManager.cs
+++ DesktopCS/Services/TabManager.cs
@@ -12,30 +12,30 @@ namespace DesktopCS.Services
 {
     public class TabManager
     {
-        public ObservableCollection<CSTabItem> ServerTabs { get; private set; }
-        public ObservableCollection<CSTabItem> ChannelTabs { get; private set; }
-        public ObservableCollection<CSTabItem> UserTabs { get; private set; }
+        private readonly ObservableCollection<CSTabItem> _serverTabs = new ObservableCollection<CSTabItem>();
+        private readonly ObservableCollection<CSTabItem> _channelTabs = new ObservableCollection<CSTabItem>();
+        private readonly ObservableCollection<CSTabItem> _userTabs = new ObservableCollection<CSTabItem>();
         private readonly Dictionary<string, Tab> _tabDictionary = new Dictionary<string, Tab>();
+
+        public CompositeCollection<CSTabItem> Tabs { get; private set; }
 
         public TabManager()
         {
-            this.ServerTabs = new ObservableCollection<CSTabItem>();
-            this.ChannelTabs = new ObservableCollection<CSTabItem>();
-            this.UserTabs = new ObservableCollection<CSTabItem>();
+            this.Tabs = new CompositeCollection<CSTabItem>(this._serverTabs, this._channelTabs, this._userTabs);
         }
 
         void channel_Close(object sender, EventArgs e)
         {
             var tab = (Tab)sender;
             this._tabDictionary.Remove(tab.Header);
-            this.ChannelTabs.Remove(tab.TabItem);
+            this._channelTabs.Remove(tab.TabItem);
         }
 
         void user_Close(object sender, EventArgs e)
         {
             var tab = (Tab)sender;
             this._tabDictionary.Remove(tab.Header);
-            this.UserTabs.Remove(tab.TabItem);
+            this._userTabs.Remove(tab.TabItem);
         }
 
         void user_HeaderChange(object sender, string oldValue)
@@ -66,7 +66,7 @@ namespace DesktopCS.Services
 
             var server = (ServerTab)this.CreateTab((f, t) => new ServerTab(tabName, f, t));
             this._tabDictionary.Add(tabName, server);
-            this.ServerTabs.Add(server.TabItem);
+            this._serverTabs.Add(server.TabItem);
 
             return server;
         }
@@ -81,7 +81,7 @@ namespace DesktopCS.Services
             var channel = (ChannelTab)this.CreateTab((f, t) => new ChannelTab(tabName, f, t));
             channel.Close += this.channel_Close;
             this._tabDictionary.Add(tabName, channel);
-            this.ChannelTabs.Add(channel.TabItem);
+            this._channelTabs.Add(channel.TabItem);
 
             return channel;
         }
@@ -97,7 +97,7 @@ namespace DesktopCS.Services
             user.Close += this.user_Close;
             user.HeaderChange += this.user_HeaderChange;
             this._tabDictionary.Add(tabName, user);
-            this.UserTabs.Add(user.TabItem);
+            this._userTabs.Add(user.TabItem);
 
             return user;
         }
