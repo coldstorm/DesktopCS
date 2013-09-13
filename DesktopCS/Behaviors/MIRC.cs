c41fac46d750eb4aea6cb99e530d014530910cf0
Yonom <sepehr1376@gmail.com>
2013-09-13 13:52:44 +0200

    Add fomatting support to topic

---
+++ DesktopCS/Behaviors/MIRC.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using DesktopCS.Helpers;
+
+namespace DesktopCS.Behaviors
+{
+    internal class MIRC
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(
+            "Text",
+            typeof (string),
+            typeof (MIRC),
+            new PropertyMetadata(null, OnTextChanged)
+            );
+
+        public static string GetText(DependencyObject d)
+        {
+            return d.GetValue(TextProperty) as string;
+        }
+
+        public static void SetText(DependencyObject d, string value)
+        {
+            d.SetValue(TextProperty, value);
+        }
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = d as Span;
+            if (textBlock == null)
+                return;
+
+            textBlock.Inlines.Clear();
+
+            var newText = (string)e.NewValue;
+            if (string.IsNullOrEmpty(newText))
+                return;
+
+            textBlock.Inlines.Add(MIRCHelper.Parse(newText, ColorHelper.MessageColor));
+        }
+    }
+}
