ebdbf642fab5e3956e462ae4669485ee4f7ed19b
Yonom <sepehr1376@gmail.com>
2014-01-05 13:36:57 +0100

    Add SoundHelper, Add ping sound

--- DesktopCS/Helpers/Parsers/HighlightHelper.cs
+++ DesktopCS/Helpers/Parsers/HighlightHelper.cs
@@ -3,21 +3,26 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
+using System.Windows.Navigation;
 
 namespace DesktopCS.Helpers.Parsers
 {
     public static class HighlightHelper
     {
-
         public static Span Parse(string text, ParseArgs args, Func<string, Inline> callback)
         {
             var highlightRegex = new Regex(String.Format(@"\b{0}\b", args.HostNickname), RegexOptions.IgnoreCase);
 
             return RegexHelper.Parse(text, highlightRegex, args, callback,
-                s => new Run(s)
+                s =>
                 {
-                    FontWeight = FontWeights.Bold,
-                    Foreground = new SolidColorBrush(ColorHelper.HighlightColor)
+                    SoundHelper.PlaySound(SoundHelper.PingUri);
+
+                    return new Run(s)
+                    {
+                        FontWeight = FontWeights.Bold,
+                        Foreground = new SolidColorBrush(ColorHelper.HighlightColor)
+                    };
                 });
         }
     }
---
+++ DesktopCS/Helpers/SoundHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows;
+
+namespace DesktopCS.Helpers
+{
+    public static class SoundHelper
+    {
+        public const string PingUri = @"pack://application:,,,/Resources/cs_ping.wav";
+
+        public static void PlaySound(string uriPath)
+        {
+            var uri = new Uri(uriPath);
+            var streamResourceInfo = Application.GetResourceStream(uri);
+            if (streamResourceInfo != null)
+            {
+                var player = new SoundPlayer(streamResourceInfo.Stream);
+                player.Play();
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+}
