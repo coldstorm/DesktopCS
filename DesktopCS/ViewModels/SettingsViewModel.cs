ac76b16cfa8632dd9b2a218f11c0f9820bdc62dd
Express50 <press.ex50@gmail.com>
2014-03-11 11:48:17 -0400

    Working on settings view
    - Use a StackPanel with nested horizontal StackPanels for settings
    - Add SettingsViewModel for SettingsData

---
+++ DesktopCS/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DesktopCS.Models;
+using DesktopCS.MVVM;
+using DesktopCS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesktopCS.ViewModels
+{
+    class SettingsViewModel : ObservableObject
+    {
+        public IRCSettings SettingsData { get; private set; }
+
+        public SettingsViewModel()
+        {
+            this.SettingsData = SettingsManager.Value.GetIRCSettings();
+        }
+    }
+}
