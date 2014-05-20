7f336c810969bd8656335637df0aaa2fd0ce8025
Express50 <press.ex50@gmail.com>
2014-05-20 14:27:34 -0400

    AppVeyor: List files in deployment folder after copying

--- deploy.ps1
+++ deploy.ps1
@@ -1,22 +1,24 @@
+$deploymentFolder = "C:\projects\desktopcs\Deployment"
+$projectFile = "C:\projects\desktopcs\DesktopCS\DesktopCS.csproj"
+$releaseFolder = "C:\projects\desktopcs\DesktopCS\bin\Release\app.publish"
 $version = $env:APPVEYOR_BUILD_VERSION
 $gitExe = Get-Command git -syntax
 $gitClone = "clone --branch=gh-pages https://github.com/coldstorm/DesktopCS.git " + $deploymentFolder
 $gitFormatPatch = "format-patch --stdout HEAD^"
 $patchFile = "deploy-" + $version + ".patch"
-$deploymentFolder = "C:\projects\desktopcs\Deployment"
-$projectFile = "C:\projects\desktopcs\DesktopCS\DesktopCS.csproj"
-$releaseFolder = "C:\projects\desktopcs\DesktopCS\bin\Release\app.publish"
-
-mkdir ($deploymentFolder + "\download\")
 
 Start-Process -FilePath $gitExe -ArgumentList $gitClone -Wait -NoNewWindow
 
 & 'C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe' /target:publish /p:Configuration=Release /p:Platform=AnyCPU /p:ApplicationVersion=$version $projectFile
 
+cd $deploymentFolder
+
+mkdir "\download"
+
 Move-Item -literalPath ($releaseFolder + "\Application Files") -destination ($deploymentFolder + "\download\")
 Move-Item -literalPath ($releaseFolder + "\DesktopCS.application") -destination ($deploymentFolder + "\download\")
 
-cd $deploymentFolder
+ls $deploymentFolder -Recurse
 
 git add .
 
