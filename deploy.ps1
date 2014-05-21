d14fac057ae5cfda136932dc31bb51e9fddbaac6
Express50 <press.ex50@gmail.com>
2014-05-20 20:33:52 -0400

    AppVeyor: Try copying the whole release folder

--- deploy.ps1
+++ deploy.ps1
@@ -17,8 +17,7 @@ cd $deploymentFolder
 
 mkdir "\download"
 
-Move-Item -Path ($releaseFolder + "\Application Files\") -Destination ($deploymentFolder + "\download") -Force -Verbose
-Move-Item -Path ($releaseFolder + "\DesktopCS.application") -Destination ($deploymentFolder + "\download") -Force -Verbose
+Move-Item -Path ($releaseFolder + "\*") -Destination ($deploymentFolder + "\download") -Force -Verbose
 
 ls $deploymentFolder -Recurse -Force
 
