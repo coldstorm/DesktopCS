3cf62a2a83669196284558e598b90fac8088ce68
Express50 <press.ex50@gmail.com>
2014-06-23 12:07:51 -0400

    Add ImageHelper class

---
+++ DesktopCS/Helpers/Parsers/ImageHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+
+namespace DesktopCS.Helpers.Parsers
+{
+    public static class ImageHelper
+    {
+        public static Span Parse(string text, ParseArgs args, Func<string, Inline> callback)
+        {
+            return RegexHelper.Parse(text, _imageRegex, args, callback,
+                s =>
+                {
+                    BitmapImage bi = new BitmapImage(new Uri(s));
+                    Image image = new Image();
+                    image.Source = bi;
+                    InlineUIContainer container = new InlineUIContainer(image);
+                    return container;
+                });
+        }
+
+        private static readonly Regex _imageRegex = new Regex(@"(.+?).(png|tif|jpg|jpeg|bmp|gif)", RegexOptions.IgnoreCase);
+    }
+}
